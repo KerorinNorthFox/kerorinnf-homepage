e908075fb3ad6f63d6fe2efc1a52d9c934d98641
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-06-19 13:05:32 +0900

    画面の端にスクロールバーの幅だけ余分な隙間ができる問題を修正

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import FooterSiteInfo from "./FooterSiteInfo";
 const Footer = () => {
   return(
     <footer>
-      <div className="w-screen h-64 pt-12 bg-[#e5e7eb] text-gray-500">
-        <div className="max-w-[60%] mx-auto px-4 hidden lg:flex justify-between">
+      <div className="h-64 pt-12 bg-[#e5e7eb] text-gray-500">
+        <div className="max-w-[70%] mx-auto px-4 hidden lg:flex justify-between">
           <FooterNav />
           <FooterSiteInfo />
         </div>
