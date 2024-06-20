7d155b6c8ebc6b54d5e44b3caf481bee920b164e
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-06-20 16:46:10 +0900

    sm以下でトグルボタンが動作しないバグを解決

--- src/components/Header/NavDrawer.tsx
+++ src/components/Header/NavDrawer.tsx
@@ -4,35 +4,36 @@ const NavDrawer = () => {
   return (
     <div
       id="drawer-navigation"
-      className="fixed right-0 top-0 z-40 h-screen w-64 translate-x-full overflow-y-auto bg-light-bg p-4 text-light-text drop-shadow-2xl transition-transform dark:bg-dark-bg dark:text-dark-text"
+      className="fixed right-0 top-0 z-40 h-screen w-64 translate-x-full divide-y divide-gray-200 overflow-y-auto bg-light-bg p-4 text-light-text drop-shadow-2xl transition-transform dark:divide-gray-600 dark:bg-dark-bg dark:text-dark-text"
       tabIndex={-1}
       aria-labelledby="drawer-navigation-label">
-      <h5
-        id="drawer-navigation-label"
-        className="ml-4 font-semibold">
-        Menu
-      </h5>
-      <button
-        className="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-500 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-        type="button"
-        data-drawer-hide="drawer-navigation"
-        aria-controls="drawer-navigation">
-        <svg
-          className="h-3 w-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14">
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-          />
-        </svg>
-      </button>
-      <hr className="mt-4" />
+      <div className="pb-4">
+        <h5
+          id="drawer-navigation-label"
+          className="ml-4 font-semibold">
+          Menu
+        </h5>
+        <button
+          className="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-500 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+          type="button"
+          data-drawer-hide="drawer-navigation"
+          aria-controls="drawer-navigation">
+          <svg
+            className="h-3 w-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14">
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+      </div>
       <div className="overflow-y-auto">
         <ul>
           <NavDrawerContent>
