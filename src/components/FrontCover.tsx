61bc68111bb4c3162f51bc49373622e5fcbb8c8b
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-09-01 23:18:04 +0900

    ホーム画面を作成

--- src/components/FrontCover.tsx
+++ src/components/FrontCover.tsx
@@ -1,7 +1,23 @@
 const FrontCover = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <h1>Top page</h1>
+      <div className="md:w-[50%]">
+        <div className="space-y-4 text-center">
+          <h1 className="text-4xl">けろりん北狐のホームページ</h1>
+          <p className="text-lg">何も書くことがねぇ</p>
+        </div>
+        <img
+          src="https://count.getloli.com/get/@kerorinnf?theme=rule34"
+          alt="access counter"
+          className="mx-auto mt-24"
+        />
+        <p className="text-center text-xs text-gray-500">ケモ耳可愛い</p>
+      </div>
+      <div className="hidden w-[50%] md:flex">
+        <div className="m-auto border border-light-text text-center dark:border-dark-text">
+          <p className="p-12">自己紹介エリア</p>
+        </div>
+      </div>
     </div>
   );
 };
