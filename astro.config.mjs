04c197a6d56d79d9f4b80c608c4543239d040c15
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-09-04 19:09:32 +0900

    feat:クエリパラメーターでブログの記事をタグでフィルターできる機能を実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kerorinnf.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  site: "https://kerorinnf.vercel.app",
+  output: "hybrid",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
