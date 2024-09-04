32bd5311eb77f1b66d71425e966eda448d378166
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-09-04 12:12:45 +0900

    footerのUIが崩れているところを修正

--- src/components/Footer/FooterSiteInfo.tsx
+++ src/components/Footer/FooterSiteInfo.tsx
@@ -3,12 +3,12 @@ import SocialMediaLinks from "./SocialMediaLinks";
 const FooterSiteInfo = () => {
   const today = new Date();
   return (
-    <>
+    <div>
       &copy; {today.getFullYear()} KerorinNF. All rights reserved.
       <div className="mt-4 flex justify-center lg:justify-start lg:pl-4">
         <SocialMediaLinks />
       </div>
-    </>
+    </div>
   );
 };
 
