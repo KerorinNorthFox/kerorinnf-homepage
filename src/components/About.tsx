d2ab606d6f7315b22f52f1d73af1499162283637
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2025-03-22 14:21:11 +0900

    aboutとheader編集

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,24 @@
 const About = () => {
   return (
-    <div className="flex h-[560px] w-full justify-center">
-      <div className="my-12 w-[95%] rounded-xl border border-black bg-white py-16 shadow-lg lg:w-[60%]">
-        <div className="font-bold text-light-text">
-          <h1 className="text-center text-4xl">Profile</h1>
-          <div className="mt-12 grid grid-cols-4 gap-y-4 px-12 text-2xl">
-            <p className="col-span-1 text-right">Name：</p>
-            <p className="col-span-3 ml-8">KerorinNF</p>
-            <p className="col-span-1 text-right">Age：</p>
-            <p className="col-span-3 ml-8">19yo</p>
-            <p className="col-span-1 text-right">Hobbies：</p>
-            <p className="col-span-3 ml-8">Programming, Osu!</p>
-            <p className="col-span-1 text-right">Skills：</p>
-            <p className="col-span-3 ml-8">Python, Nim, C#, Flutter, Nextjs</p>
-          </div>
-        </div>
-      </div>
+    // <div className="flex h-[560px] w-full justify-center">
+    //   <div className="my-12 w-[95%] rounded-xl border border-black bg-white py-16 shadow-lg lg:w-[60%]">
+    //     <div className="font-bold text-light-text">
+    //       <h1 className="text-center text-4xl">Profile</h1>
+    // <div className="mt-12 grid grid-cols-4 gap-y-4 px-12 text-2xl">
+    //   <p className="col-span-1 text-right">Name：</p>
+    //   <p className="col-span-3 ml-8">KerorinNF</p>
+    //   <p className="col-span-1 text-right">Age：</p>
+    //   <p className="col-span-3 ml-8">19yo</p>
+    //   <p className="col-span-1 text-right">Hobbies：</p>
+    //   <p className="col-span-3 ml-8">Programming, Osu!</p>
+    //   <p className="col-span-1 text-right">Skills：</p>
+    //   <p className="col-span-3 ml-8">Python, Nim, C#, Flutter, Nextjs</p>
+    // </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="border border-light-text p-12 text-center dark:border-dark-text">
+      <h1>こんにちは</h1>
     </div>
   );
 };
