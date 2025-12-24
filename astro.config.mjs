597e03ad10612306a0b6748a9a2dd46d089eb046
kerorinnf <kerorinnf@gmail.com>
2025-12-24 11:15:18 +0900

    feat: astroのmdxインテグレーションをインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +15,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [react()],
+  integrations: [react(), mdx()],
 });
