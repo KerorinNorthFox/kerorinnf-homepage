6df1636dd101cbaf89d8c806c09813925a7dd2e4
kerorinnf <kerorinnf@gmail.com>
2026-01-11 19:19:42 +0900

    feat: github pagesにデプロイ. feat: カスタムドメインを設定.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://kerorinnf.com",
   vite: {
     plugins: [tailwindcss()],
     server: {
